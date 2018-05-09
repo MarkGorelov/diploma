47b8ea22ddd5f0713a5ec211a0ad1fe1e6c7ae7b
Gorelov Mark <gorelov.mark@protonmail.com>
2018-05-09 16:53:12 +0300

    Add CompanyPage

---
+++ controllers/CompanyController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 09.05.2018
+ * Time: 15:53
+ */
+
+class CompanyController
+{
+
+}
---
+++ views/company/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 09.05.2018
+ * Time: 15:56
+ */
